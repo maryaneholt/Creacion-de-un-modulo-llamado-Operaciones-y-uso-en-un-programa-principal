39e462471b3dbf1e7ab282c9665d96b821346226
Maryane Holt <maryane.holt@uip.edu.pa>
2026-04-09 22:05:06 -0500

    Se le agregó funciones aritméticas básicas en operaciones.py

---
+++ operaciones.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Resultado indefinido: No es posible dividir entre cero."
+    return a / b
